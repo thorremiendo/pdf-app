4658c66ce99e36cf26df70e475bb121563eab0bd
Thor Remiendo <thorremiendo@gmail.com>
2020-12-11 21:20:58 +0800

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxDropzoneChangeEvent } from 'ngx-dropzone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pdf-app';
+  public formData = {};
+
+
+  constructor() {}
+
+  ngOnInit() {}
+ 
 }
